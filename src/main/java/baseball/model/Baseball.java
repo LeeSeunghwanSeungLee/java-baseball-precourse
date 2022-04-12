af2a3927169f8aa33d7c41dffbc91ac9dcdcad24
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-12 21:43:59 +0900

    feat: Model -> Baseball 기능 추가&& 테스트 코드 수정
    getter && compare 로직 추가

--- src/main/java/baseball/model/Baseball.java
+++ src/main/java/baseball/model/Baseball.java
@@ -12,4 +12,21 @@ public class Baseball {
     public boolean isValid() {
         return no <= 9 && no >0;
     }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public int getNumber() {
+        return this.no;
+    }
+
+    public BallScore compare(Baseball ball) {
+        if (this.no == ball.getNumber() && this.position == ball.getPosition())
+            return BallScore.Strike;
+        else if (this.no == ball.getNumber() && this.position != ball.getPosition())
+            return BallScore.Ball;
+
+        return BallScore.Nothing;
+    }
 }
--- src/test/java/baseball/model/BaseballTest.java
+++ src/test/java/baseball/model/BaseballTest.java
@@ -22,4 +22,61 @@ class BaseballTest {
 
         assertThat(numberUpperErrorBall.isValid()).isFalse();
     }
+
+    @Test
+    @DisplayName("Strike 검증")
+    void 스트라이크테스트() {
+        // given
+        Baseball answer = new Baseball(1, 5);
+        Baseball checkRight  = new Baseball(1, 5);
+        Baseball checkWrong_1 = new Baseball(2, 5);
+        Baseball checkWrong_2 = new Baseball(1, 9);
+
+        // when
+        BallScore right = answer.compare(checkRight);
+        BallScore wrong_1 = answer.compare(checkWrong_1);
+        BallScore wrong_2 = answer.compare(checkWrong_2);
+
+        // then
+        assertThat(right).isEqualTo(BallScore.Strike);
+        assertThat(wrong_1).isNotEqualTo(BallScore.Strike);
+        assertThat(wrong_2).isNotEqualTo(BallScore.Strike);
+    }
+
+    @Test
+    @DisplayName("Ball 테스트")
+    void 볼테스트() {
+        // given
+        Baseball answer = new Baseball(1, 2);
+        Baseball check = new Baseball(2, 2);
+
+        // when
+        BallScore score = answer.compare(check);
+        // then
+        assertThat(score).isEqualTo(BallScore.Ball);
+    }
+
+    @Test
+    @DisplayName("낫싱 테스트")
+    void 나싱테스트() {
+        // given
+        Baseball answer = new Baseball(1, 5);
+        Baseball checkWrong_0  = new Baseball(2, 1);
+        Baseball checkWrong_1 = new Baseball(3, 8);
+        Baseball checkWrong_2 = new Baseball(5, 2);
+        Baseball checkBall = new Baseball(3, 5);
+
+        // when
+        BallScore wrong_0 = answer.compare(checkWrong_0);
+        BallScore wrong_1 = answer.compare(checkWrong_1);
+        BallScore wrong_2 = answer.compare(checkWrong_2);
+        BallScore ball = answer.compare(checkBall);
+
+        // then
+        assertThat(wrong_0).isEqualTo(BallScore.Nothing);
+        assertThat(wrong_1).isEqualTo(BallScore.Nothing);
+        assertThat(wrong_2).isEqualTo(BallScore.Nothing);
+
+        assertThat(ball).isNotEqualTo(BallScore.Nothing);
+    }
 }
