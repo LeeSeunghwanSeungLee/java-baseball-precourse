e47157eb2281a0777cbce44874f18db86a7e29fc
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-13 22:52:11 +0900

    feat: BallScore enum 클래스 메소드추가

--- src/main/java/baseball/model/BallScore.java
+++ src/main/java/baseball/model/BallScore.java
@@ -1,5 +1,23 @@
 package baseball.model;
 
 public enum BallScore {
-    Strike, Ball, Nothing
+    Strike(0), Ball(1), Nothing(2);
+
+    private final int value;
+
+    BallScore(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public boolean isStrike() {
+        return this == Strike;
+    }
+
+    public boolean isBall() {
+        return this == Ball;
+    }
 }
