ddb857530f35acca0738df978f8fdd5f8a532f26
lshn1007 <lshn1007@hanyang.ac.kr>
2022-04-15 19:25:29 +0900

    feat: controller 추가
    main에 컨트롤러 추가 및 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import baseball.controller.PlayController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("version setting");
+        PlayController controller = new PlayController();
+        controller.start();
     }
 }
--- src/main/java/baseball/controller/PlayController.java
+++ src/main/java/baseball/controller/PlayController.java
@@ -1,2 +1,72 @@
-package baseball.controller;public class PlayController {
+package baseball.controller;
+
+import baseball.model.PlayResult;
+import baseball.repository.PlayInning;
+import baseball.utils.InputConsole;
+import baseball.utils.InputValidator;
+import baseball.utils.RandomNumber;
+import baseball.view.View;
+
+import java.util.List;
+
+public class PlayController {
+    private PlayInning computerBalls;
+    private List<Integer> userBalls;
+
+    public void start() {
+        // 랜덤 결과를 생성한다 -> utils/RandomNumber
+        computerBalls = new PlayInning(RandomNumber.make());
+
+        play();
+    }
+
+    private void play() {
+        try {
+            View.EnterNumber.print();
+            String consoleString = InputConsole.getStringFromConsole();
+
+            InputValidator.validateInputType(consoleString);
+            InputValidator.validateIsEmpty(consoleString);
+            InputValidator.validateSize(consoleString);
+            InputValidator.validateNumberRange(consoleString);
+            InputValidator.validateEqualNumber(consoleString);
+
+            userBalls = InputValidator.changeStringToNumberList(consoleString);
+
+            PlayResult pr = this.computerBalls.play(userBalls);
+            View.playResult(pr.toString());
+            isWin(pr);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            play();
+        }
+    }
+
+    private void isWin(PlayResult pr) {
+        if (pr.isGameEnd()) {
+            View.GameEnd.print();
+            View.RestartGameOrQuit.print();
+            restartOrQuit();
+            return;
+        }
+        play();
+    }
+
+    private void restartOrQuit() {
+        try {
+            isGameEnd();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            restartOrQuit();
+        }
+    }
+
+    private void isGameEnd() {
+        String userType = InputConsole.getStringFromConsole();
+        if (userType.equals("1"))
+            start();
+        else if (userType.equals("2"))
+            System.exit(0);
+        throw new IllegalArgumentException("[ERROR] -> 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
